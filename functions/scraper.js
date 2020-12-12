7386706531e9d9909a946f4d3a11c3c3132da94b
Pierre-Emmanuel DENYS <pierreemmanuel.denys@gmail.com>
2020-12-12 10:08:10 +0100

    Remove crendentials header

--- functions/scraper.js
+++ functions/scraper.js
@@ -5,7 +5,6 @@ const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
   'Access-Control-Allow-Headers': 'Content-Type',
-  'Access-Control-Allow-Credentials': true,
   'Content-Type': 'application/json; charset=utf-8',
 }
 
